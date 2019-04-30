d9baa1e9a207290a7c2df687b66459233c81aa89
rm2rl <rmdwirizki@gmail.com>
2019-04-30 09:27:08 +0700

    Update dist modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const { resolve } = require('path');
-const createDefaultConfig = require('@open-wc/building-webpack/modern-and-legacy-config');
+const createDefaultConfigs = require('@open-wc/building-webpack/modern-and-legacy-config');
 
 // If you don't need IE11 support, use the modern-config instead
 // import createDefaultConfig from '@open-wc/building-webpack/modern-config';
 
-module.exports = createDefaultConfig({
+const configs = createDefaultConfigs({
   input: resolve(__dirname, './src/index.html'),
 });
+
+// module.exports = createDefaultConfig({
+//   input: resolve(__dirname, './src/index.html'),
+// });
+
+// Using loader example
+// module.exports = configs.map(config =>
+//   merge(config, {
+//     module: {
+//       rules: [{ test: /\.ts$/, loader: 'ts-loader' }],
+//     },
+//   }),
+// );
+
+const development = !process.argv.find(arg => arg.includes('production'));
+module.exports = development ? configs : configs.map(
+  (config, legacy) => Object.assign(config, {
+    output: {
+      filename: `${legacy ? 'legacy/' : ''}index.js`,
+      chunkFilename: `${legacy ? 'legacy/' : ''}[name].js`,
+      path: resolve(__dirname, './dist')
+    },
+  })
+)
